f446389ce7c6274635b97a40b73b7717c889a13d
Guilherme Sposito <guilhermesposito14@gmail.com>
2023-07-14 18:23:35 -0300

    mudancas a serem testadas depois

--- index.js
+++ index.js
@@ -1,3 +1,82 @@
-console.log('Teste, Teste, Teste');
+class Nomes {
 
-console.log('mudando o arquivo .js');
+    constructor(nome) {
+        this.nome = nome
+    }
+
+
+}
+
+
+class Bg {
+
+    constructor() {
+        if (localStorage.getItem('id') == null) {
+            localStorage.setItem(0, 0)
+        }
+    }
+
+    insereNoStorage(p) {
+
+        localStorage.setItem(2, JSON.stringify(p))
+
+    }
+
+    recebeDoStorage() {
+
+        let nomes = localStorage.getItem(1)
+        nomes = JSON.parse(nomes)
+        let nome = Array(nomes)
+
+        return nome
+    }
+
+}
+
+let bg = new Bg()
+
+
+function salvaNome() {
+    let nomeInserido = document.getElementById('idtxt').value
+    let objctNome = new Nomes(nomeInserido)
+
+    bg.insereNoStorage(objctNome)
+
+    console.log(objctNome)
+}
+
+
+function mostraNome() {
+
+    //cria modal 
+    let modal = document.createElement('div')
+    modal.style.width = '500px'
+    modal.style.height = '500px'
+    modal.style.backgroundColor = 'black'
+
+    modal.style.color = 'white'
+    modal.id = 'modal'
+    modal.addEventListener('click', fecharModal)
+
+
+
+    window.document.body.appendChild(modal)
+
+    let nomeArray = bg.recebeDoStorage()
+
+    nomeArray.forEach(p => {
+
+        modal.innerText = p.nome
+
+
+    })
+
+
+}
+
+
+function fecharModal() {
+
+    document.getElementById('modal').remove()
+
+}
